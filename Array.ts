b91e14aab181db378fbdbdcf8b74951b19e9fc81
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-16 17:17:42 +0700

    problem 1 Compare String

---
+++ Array.ts
@@ -0,0 +1,37 @@
+function compareString(a, b) {
+    String a = "AKA";
+    Sting b = "AKASHI"
+
+    if (a.equals(b) == 0)
+    console.log(" ")
+
+    String a = "KANGAROO";
+    Sting b = "KANG"
+
+    if (a.equals(b) == 0)
+    console.log(" ")
+
+    String a = "KI";
+    Sting b = "KIJANG"
+
+    if (a.equals(b) == 0)
+    console.log(" ")
+
+    String a = "KUPU-KUPU";
+    Sting b = "KUPU"
+
+    if (a.equals(b) == 0)
+    console.log(" ")
+
+    String a = "ILALANG";
+    Sting b = "ILA"
+
+    if (a.equals(b) == 0)
+    console.log(" ")
+
+    console.log(compareString("AKA", "AKASHI"))
+    console.log(compareString("KANGAROO", "KANG"))
+    console.log(compareString("KI", "KIJANG"))
+    console.log(compareString("KUPU-KUPU", "KUPU"))
+    console.log(compareString("ILALANG", "ILA"))
+}
